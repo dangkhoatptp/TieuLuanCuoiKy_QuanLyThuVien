12da5b9c1575f7e9d39fed5b8b4daa30154bd194
dangkhoatptp <73412991+dangkhoatptp@users.noreply.github.com>
2022-06-19 09:31:01 +0700

    .

--- QuanLyThuVien/QuanLyThuVien/Properties/AssemblyInfo.cs
+++ QuanLyThuVien/QuanLyThuVien/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WindowsFormsApp1")]
+[assembly: AssemblyTitle("QuanLyThuVien")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WindowsFormsApp1")]
+[assembly: AssemblyProduct("QuanLyThuVien")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ee02e49d-ae39-4fe3-97b4-260e1d9f88e2")]
+[assembly: Guid("2de27cb4-464c-4c60-80b3-c809689b0a77")]
 
 // Version information for an assembly consists of the following four values:
 //
